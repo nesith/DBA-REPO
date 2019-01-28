e18b50c87623b97da622eb50b6e5d73e13762b65
Nesith <nesithw@gmail.com>
2019-01-28 19:41:20 +1000

    Initial Commit

---
+++ WINCompResources/DSCResources/DomainController/DomainController.schema.ps1
@@ -0,0 +1,10 @@
+#This file contains the definition of a domain controller which can be created through DSC.
+#Since this is a composite resource, this can be used in any DSC config file after importing it.
+
+Configuration DomainController
+{
+    Param
+    (
+        
+    )
+}
