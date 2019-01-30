01ce8734a166a9747fdc5b78f11fc3c1d2c35486
Nesith Weerasinghe (Admin) <nweerasinghe_Admin@bne.com.au>
2019-01-30 13:59:08 +1000

    Initial Commit

---
+++ T-SQL Scripts/sql_mon_index_frag.sql
@@ -0,0 +1,18 @@
+--This script list all index fragmentations as found in Redgate SQL Monitor Database
+
+SELECT 
+    A._Fragmentation AS [Fragmentation],
+    B._Name AS [Index Name],
+    c._Schema AS [Schema],
+    c._Name AS [Table Name],
+    D._Name AS [Database Name],
+    E._Name AS [Node],
+    F._Name AS [Cluster/Server] 
+FROM [data].[Cluster_SqlServer_Database_Table_Index_UnstableSamples] A
+JOIN [data].[Cluster_SqlServer_Database_Table_Index_Keys] B ON B.Id = A.Id
+JOIN [data].[Cluster_SqlServer_Database_Table_Keys] C ON C.Id = B.ParentId
+JOIN [data].[Cluster_SqlServer_Database_Keys] D ON D.Id = C.ParentId
+JOIN [data].[Cluster_SqlServer_Keys] E ON E.Id = D.ParentId
+JOIN [data].[Cluster_Keys] F ON F.id = E.ParentId	
+--WHERE a._Fragmentation > 0.8 AND D._Name like 'Red%' 
+ORDER BY Fragmentation DESC
